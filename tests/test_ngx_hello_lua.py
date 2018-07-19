dba1343a1ed38ed1fdfb9f5f8b0c27cf2bfa7cc4
阏男秀 <pl01665077@163.com>
2018-07-19 08:55:00 +0800

    update test

--- tests/test_ngx_hello_lua.py
+++ tests/test_ngx_hello_lua.py
@@ -9,28 +9,28 @@ curr_dir = os.path.abspath(os.path.dirname(__file__))
 HOST = "127.0.0.1"
 PORT = 10000
 
-BASE_URL = "http://{host}:{port}".format(host=HOST,port=PORT)
+BASE_URL = "http://{host}:{port}".format(host=HOST, port=PORT)
 
 
 class TestNgxHello(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_A_index(self):
+    def tearDown(self):
+        pass
+
+    def test_1_index(self):
         """测试OpenResty主页"""
         url = BASE_URL+"/"
         response = requests.get(url)
-        self.assertTrue(200==response.status_code)
+        self.assertTrue(200 == response.status_code)
 
-    def test_B_lua(self):
+    def test_2_lua(self):
         """测试hello lua页面"""
         url = BASE_URL+"/lua"
         response = requests.get(url)
-        self.assertTrue(200==response.status_code)
+        self.assertTrue(200 == response.status_code)
         self.assertTrue("Lua" in response.text)
-
-    def tearDown(self):
-        pass
 
 
 if __name__ == '__main__':
